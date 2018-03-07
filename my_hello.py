a7b8383ca8c55972b5aaf1da7c76d0feb1a67b9e
MissLanLan <jiangbaolan@163.com>
2018-03-07 00:19:44 -0800

    20180307 16:16 jinjia2+Bootstrap+static+moment

--- my_hello.py
+++ my_hello.py
@@ -1,21 +1,35 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask_script import Manager,Server
+from flask_bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
+
+moment = Moment(app)
 
 manager = Manager(app)
 manager.add_command("runserver",Server(use_debugger=True))
 
+bootstrap = Bootstrap(app)
+
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('my_index.html',current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('my_user.html',name=name)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('my_404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('my_500.html'),500
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(host='192.168.163.128')
